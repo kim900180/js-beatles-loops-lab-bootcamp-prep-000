75c704da4457b7be2ffce6825d6f8925ae7aee09
Jae Yong Kim <kim900180@gmail.com>
2018-07-11 01:20:50 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 function theBeatlesPlay(musicians, instruments) {
   var array = [];
   for (var i = 0; i < musicians.length - 1; i++) {
-    array.push(musicians[i] + "plays" + instruments[i]);
+    array.push(musicians[i] + " plays " + instruments[i]);
   }
   return array
 }
